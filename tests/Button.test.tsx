7f66c6fcedb6bc01ade55c178895d9d62de13189
nickderaj <nickderaj@gmail.com>
2022-07-21 16:34:07 +0800

    test: added simple unit test for sample button

---
+++ tests/Button.test.tsx
@@ -0,0 +1,26 @@
+import Button, { IButton } from '@/components/buttons/SampleButton';
+import '@testing-library/jest-dom';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+describe('Sample Button', () => {
+  // Initialise props
+  let expectedProps: IButton;
+
+  // Give props sample values
+  beforeEach(() => {
+    expectedProps = {
+      title: 'sample',
+      onClick: jest.fn(),
+    };
+  });
+
+  // Run Tests
+  it('is created and function called on click', () => {
+    render(<Button {...expectedProps} />);
+    const button = screen.getByText(expectedProps.title);
+
+    expect(button).toBeVisible();
+    fireEvent.click(button);
+    expect(expectedProps.onClick).toBeCalledTimes(1);
+  });
+});
