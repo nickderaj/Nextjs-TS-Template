33e37d204cc2ad2faedf3e1b21f5d189a29ba713
Nicholas De Raj <nickderaj@gmail.com>
2023-07-16 11:07:16 +0800

    feat: update metadata to include twitter

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -1,15 +1,32 @@
 import { Metadata } from 'next';
 
-const title = 'Next.js Template';
-const description = 'Next.js Template';
+const metadataConfig = {
+  title: 'Next.js Template',
+  description: 'Next.js Template',
+  url: 'https://nextjs.org/',
+  image: '/favicon.ico',
+  twitter: 'https://twitter.com/nickderaj',
+  email: 'nickderaj@gmail.com',
+  github: 'https://github.com/nickderaj',
+  facebook: 'https://www.facebook.com/nickderaj/',
+  youtube: 'https://youtube.com/nickderaj',
+  linkedin: 'https://www.linkedin.com/in/nickderaj/',
+};
 
 export const SharedMetadata: Metadata = {
-  title,
-  description,
+  robots: 'follow, index',
+  title: metadataConfig.title,
+  description: metadataConfig.description,
   openGraph: {
-    title,
-    description,
-    // url: 'https://nextjs.org/',
-    images: [{ url: '/favicon.ico', width: 256, height: 256, alt: 'favicon' }],
+    title: metadataConfig.title,
+    description: metadataConfig.description,
+    url: metadataConfig.url,
+    images: [{ url: metadataConfig.image, width: 256, height: 256, alt: 'favicon' }],
+  },
+  twitter: {
+    title: metadataConfig.title,
+    description: metadataConfig.description,
+    site: metadataConfig.twitter,
+    images: [{ url: metadataConfig.image, width: 256, height: 256, alt: 'favicon' }],
   },
 };
