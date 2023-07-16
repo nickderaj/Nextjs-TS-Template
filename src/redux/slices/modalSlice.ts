5e2d933421be60eaf0846f920585b14a4caa78f2
Nicholas De Raj <nickderaj@gmail.com>
2023-07-16 10:45:29 +0800

    fix: pnpm issue with redux toolkit

--- src/redux/slices/modalSlice.ts
+++ src/redux/slices/modalSlice.ts
@@ -1,3 +1,7 @@
+// To fix an issue with pnpm
+import type { WritableDraft } from '.pnpm/immer@9.0.21/node_modules/immer/dist/internal';
+export type IgnoredType = WritableDraft<string>;
+
 import { IModalState } from '@/types/redux';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
